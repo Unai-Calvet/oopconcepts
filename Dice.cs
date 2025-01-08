0ddcb4a4fbdf7bd19640cd39890a8c3daf26e657
Unai Calvet <unai.calvet.7e8@itb.cat>
2025-01-08 20:59:40 +0100

    Exemple One closes #1

---
+++ Dice.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace oopconcepts {
+    public class Dice {
+        public string Color { get; set; }
+        public int NumberOfFaces { get; set; }
+
+        public void Buy() {
+            Console.WriteLine("I'm buying a dice");
+        }
+        public void Roll() {
+            Console.WriteLine("I'm rolling a dice");
+        }
+    }
+}
